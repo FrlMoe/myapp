d00a35e3eb11b1c40b2eb2a95f335cf3eb07f7be
FrlMoe <mirjam-werner@gmx.de>
2019-03-04 18:39:28 +0100

    5.1 add routes, reroute and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
   root 'simple_pages#index'
   resources :orders, only: [:index, :show, :create, :destroy]
 
