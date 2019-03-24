5f3cccce422bd382561a72d58f1b2eddb79ade5d
FrlMoe <mirjam-werner@gmx.de>
2019-03-25 00:21:23 +0100

    5.9 add validations and pagination

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem 'devise'
 # Use for authorization
 gem 'cancancan', '~> 1.10'
 
+gem 'will_paginate', '~> 3.1', '>= 3.1.6'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
   has_many :orders
   has_many :comments
+  validates :name, presence: true
 
     def self.search(search_term)
-      if Rails .env.production?
+      if Rails.env.production?
       Product.where("name ilike ?", "%#{search_term}%")
       else
       Product.where("name LIKE ?", "%#{search_term}%")
