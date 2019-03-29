3a122d7e4f381c4be3db29e340fdc471154d5c61
FrlMoe <mirjam-werner@gmx.de>
2019-03-29 14:08:59 +0100

    6.1 add rspec

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '~> 1.3.6'
+
+  gem 'rspec-rails', '~> 3.4', '>= 3.4.2'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_104129) do
+ActiveRecord::Schema.define(version: 2019_03_29_115759) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 2019_03_25_104129) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.integer "price"
+  end
+
+  create_table "user_tests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
