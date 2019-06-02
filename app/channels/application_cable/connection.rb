06856a9036f90269b9bb34a2b955a7fba4fecce1
FrlMoe <mirjam-werner@gmx.de>
2019-06-03 01:00:35 +0200

    6.14 add ActiveJob

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,13 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+
+    end
+
+    def disconnect
+
+    end
   end
 end
